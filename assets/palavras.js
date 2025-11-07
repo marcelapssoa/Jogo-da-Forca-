ab6785ce37beb8c3df6042d17506477e5c487fc9
marcelapssoa <139571724+marcelapssoa@users.noreply.github.com>
2025-11-07 15:38:19 -0400

    Add files via upload

---
+++ assets/palavras.js
@@ -0,0 +1,41 @@
+const gameData = {
+    "titulo": "Vivendo perigosamente",
+    "palavras": [
+        {
+            "word": "UNIVERSO",
+            "dica": ""
+        },
+        {
+            "word": "SOL",
+            "dica": ""
+        },
+        {
+            "word": "TERRA",
+            "dica": ""
+        },
+        {
+            "word": "LENDAS",
+            "dica": ""
+        },
+        {
+            "word": "TEORIA",
+            "dica": ""
+        },
+        {
+            "word": "ASTRONOMIA",
+            "dica": ""
+        },
+        {
+            "word": "ORIGEM",
+            "dica": ""
+        },
+        {
+            "word": "CULTURA",
+            "dica": ""
+        },
+        {
+            "word": "TUPÃ",
+            "dica": ""
+        }
+    ]
+};
